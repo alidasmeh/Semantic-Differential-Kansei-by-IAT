bb63523ea90fee2626ff32bf33ca545a780668c1
Ali Dasmeh <dasmeh@Alis-MacBook-Air.local>
2022-04-16 18:02:49 +0430

    report array, tendency function, confidence calculation

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ document.addEventListener('keydown', function(event) {
     }
 
     if (event.keyCode == 37) {
+        // left
         temp["word"] = $("#word2").text();
         results.push(temp);
 
@@ -71,6 +72,7 @@ document.addEventListener('keydown', function(event) {
         }, global_data.times.waiting_slides);
 
     } else if (event.keyCode == 39) {
+        // right
         temp["word"] = $("#word1").text();
         results.push(temp);
 
@@ -101,11 +103,11 @@ function start() {
 }
 
 function show_image() {
-    console.log("current_trial_number", current_trial_number)
+    // console.log("current_trial_number", current_trial_number)
 
     if (current_image_number < global_data.images.length - 1) {
         current_image_number++;
-        console.log("current_image_number", current_image_number)
+        // console.log("current_image_number", current_image_number)
 
         current_image = global_data.images[current_image_number];
         timer_beginning = Date.now();
@@ -123,8 +125,63 @@ function show_image() {
 }
 
 function finished() {
-    console.log(JSON.stringify(global_data));
-    console.log(JSON.stringify(results));
+    let report = [];
+
+    global_data.images.forEach(image => {
+        global_data.words.forEach(pair_word => {
+            let array_word_one = [];
+            let array_word_two = [];
+
+            results.forEach(result => {
+                if (result.image == image) {
+                    if (result.word == pair_word.word_one) array_word_one.push(result.duration);
+                    if (result.word == pair_word.word_two) array_word_two.push(result.duration);
+                }
+            })
+
+            let total_word_one = 0;
+            array_word_one.forEach(num => { total_word_one += num })
+            if (array_word_one.length != 0) total_word_one = parseInt(total_word_one / array_word_one.length);
+
+            let total_word_two = 0;
+            array_word_two.forEach(num => { total_word_two += num })
+            if (array_word_two.length != 0) total_word_two = parseInt(total_word_two / array_word_two.length);
+
+            report.push({
+                image,
+                pair_word,
+                avg_word_one: total_word_one,
+                avg_word_two: total_word_two,
+                tendency: calc_tendency(total_word_two, total_word_one, pair_word)
+            })
+        })
+    })
+
+    console.log(report);
+    // not normilized
+
+
+    // console.log(JSON.stringify(global_data));
+    // console.log(JSON.stringify(results));
 
     alert("سپاس.");
+}
+
+function calc_tendency(avg_word_two, avg_word_one, pair_word) {
+    if (avg_word_one == 0) return `word '${pair_word.word_two}' is target. confidence : ${100} `;
+    if (avg_word_two == 0) return `word '${pair_word.word_one}' is target. confidence : ${100} `;
+
+    if (avg_word_two > avg_word_one) {
+        // tendency : word_one (avg for word one is samller)
+        let confidence = 100 - parseInt((avg_word_one / avg_word_two) * 100);
+        return `word '${pair_word.word_one}' is target. confidence : ${confidence} `;
+    }
+
+    if (avg_word_two < avg_word_one) {
+        // tendency : word_two (avg for word two is larger)
+        let confidence = 100 - parseInt((avg_word_two / avg_word_one) * 100);
+        return `word '${pair_word.word_two}' is target. confidence : ${confidence} `;
+    }
+
+    return "suspected to word averages.";
 }
